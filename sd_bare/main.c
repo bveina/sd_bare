d8080a4e6d6b97913367ebe24fbfb0c29a84b536
Ben Viall <bveina@users.noreply.github.com>
2019-10-28 15:34:00 -0400

    inital file dump, does not compile

---
+++ sd_bare/main.c
@@ -0,0 +1,18 @@
+/*
+ * sd_bare.c
+ *
+ * Created: 10/28/2019 3:25:10 PM
+ * Author : u_bviall
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
