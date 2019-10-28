fd2825f5d410ea1fcc4c14701ee7a49d24fc6863
Ben Viall <bveina@users.noreply.github.com>
2019-10-28 15:48:49 -0400

    added serial comms

--- sd_bare/main.c
+++ sd_bare/main.c
@@ -7,18 +7,33 @@
 
 #define F_CPU 16000000UL
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include <stdio.h>
+#include <avr/pgmspace.h>
 
 #include "mmc_avr.h"
 
 void initSPI();
 void initTimer();
+void initUART0_57600();
+static int uart_putchar(char c, FILE * stream);
+
+
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL,
+_FDEV_SETUP_WRITE);
 
 int main(void)
 {
 	initSPI();
 	initTimer();
-
+	initUART0_57600();
+	stdout = &mystdout;
 	
+	sei();
+	
+	printf("hello World\n");
+		
     while (1) 
     {
     }
@@ -44,4 +59,34 @@ void initTimer()
 ISR(TIMER0_COMPA_vect)
 {
 	disk_timerproc();	/* Drive timer procedure of low level disk I/O module */
+}
+
+void initUART0_57600()
+{
+	#define UCSZ_VALUE 0b011
+	UCSR0B |= (0 << RXCIE0) | (0 << TXCIE0) | (0 << UDRIE0) |
+	(0 << RXEN0) | (1 << TXEN0) | ((((UCSZ_VALUE)>>2)&1) << UCSZ02);
+	// UMSEL = uart, UPM = No Parity, USB = one stop bit
+	UCSR0C |= (0b00 <<UMSEL00) | (0b00 << UPM00) | (0 << USBS0)	|
+	((UCSZ_VALUE&0x3) << UCSZ00);
+	
+	#define BAUD 57600
+	#include <util/setbaud.h>
+	UBRR0 = UBRR_VALUE;
+	//UBRR0L = UBRRL_VALUE;
+	#if USE_2X
+	UCSR0A |= (1<< U2X0);
+	#else
+	UCSR0A &= ~(1<< U2X0);
+	#endif
+}
+
+static int uart_putchar(char c, FILE * stream)
+{
+	if (c == '\n')
+	uart_putchar('\r',stream);
+	
+	loop_until_bit_is_set(UCSR0A,UDRE0);
+	UDR0 = c;
+	return 0;
 }
